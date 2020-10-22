0193276596f7ce7b71fea9d5aba0067a5510ac88
kenta88 <solinvictus.adc@gmail.com>
2020-10-22 10:28:12 +0200

    Revert "create header"
    This reverts commit 347912d5af8b72cca316b1eeeeae422aaa113a43.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import './App.css';
-import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <h2>hello world</h2>
     </div>
   );
 }
