87684e9bf067717ea88a53cfc16df47205ea9c46
kenta88 <solinvictus.adc@gmail.com>
2020-10-27 18:50:49 +0100

    card style

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,20 @@
 import React from "react";
-
+import Character from "./Character";
 import "./Card.css";
 
 function Card() {
     return (
         <div className="card">
-            this is a card
+            <img src={Character.image} alt={Character.name} />
+            <div className="card-info">
+                <p className="card-info__name">
+                    <span>Name:</span>
+                    {Character.name}
+                </p>
+                <p className="card-info__generics">
+                    {Character.species} <span>from</span> {Character.location.name}
+                </p>
+            </div>
         </div>
     );
 }
