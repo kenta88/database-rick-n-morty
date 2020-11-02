fc9cf8b4512b5fa7f7685cbfcf815367d6975d6c
kenta88 <solinvictus.adc@gmail.com>
2020-11-02 13:50:37 +0100

    loading

--- src/App.js
+++ src/App.js
@@ -6,20 +6,42 @@ import Character from "./Components/Card/Character";
 
 import './App.css';
 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      isLoading: false,
       characters: [
         Character
       ], 
     };
   }
+
+  componentDidMount = () => {
+    this.fetchCharacters();
+  }
+
+  fetchCharacters = async () => {
+    try {
+      this.setState({isLoading: true});
+      await timeout(3000);
+    } catch(e) {
+
+    } finally {
+      this.setState({isLoading: false});
+    }
+  }
+
   render() {
+    const {characters, isLoading} = this.state;
     return (
       <div className="App">
         <Header />
-        <Grid characters={this.state.characters} />
+        <Grid characters={characters} isLoading={isLoading}/>
       </div>
     );
   }
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -4,18 +4,24 @@ import Card from "../Card/Card";
 
 import "./Grid.css";
 
-function Grid({ characters }) {
+function Grid({ characters, isLoading }) {
     return (
         <div className="container grid">
             {characters.map((character) => (
                 <Card key={character.id} character={character} />
             ))}
+            {isLoading && (
+                <div className="loading">
+                    <p>Loading ...</p>
+                </div>
+            )}
         </div>
     );
 }
 
 Grid.propTypes = {
     characters: PropTypes.array.isRequired,
+    isLoading: PropTypes.bool.isRequired,
 };
 
 export default Grid;
