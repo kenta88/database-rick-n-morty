333a6f2047c0392f2b7d3995e1284961575d76bf
kenta88 <solinvictus.adc@gmail.com>
2020-11-07 14:03:00 +0100

    add fav style

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 import "./Card.css";
 
-function Card({ character }) {
+function Card({ character, isSelected }) {
+    const selectedClass = isSelected ? "selected" : "";
     return (
-        <div className="card">
+        <div className={`card ${selectedClass}`}>
             <img src={character.image} alt={character.name} />
             <div className="card-info">
                 <p className="card-info__name">
@@ -16,12 +17,16 @@ function Card({ character }) {
                     {character.species} <span>from</span> {character.location.name}
                 </p>
             </div>
+            <button className="favourite">
+                {isSelected ? "Remove" : "Add"}
+            </button>
         </div>
     );
 }
 
 Card.propTypes = {
     character: PropTypes.object.isRequired,
+    isSelected: PropTypes.bool.isRequired,
 }
 
 export default Card;
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -5,10 +5,15 @@ import Card from "../Card/Card";
 import "./Grid.css";
 
 function Grid({ characters, isLoading }) {
+    const favourites = [1, 5];
     return (
         <div className="container grid">
             {characters.map((character) => (
-                <Card key={character.id} character={character} />
+                <Card
+                    key={character.id}
+                    character={character}
+                    isSelected={favourites.includes(character.id)}
+                />
             ))}
             {isLoading && (
                 <div className="loading">
