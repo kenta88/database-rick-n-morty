31977a746a7dbac2c8fec02803c7d8594abc6e7b
kenta88 <solinvictus.adc@gmail.com>
2020-11-07 16:07:25 +0100

    Updates

---
+++ precache-manifest.19a4919ab3415f38a715e5430529c359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f25adf8d7b2bde14c6e665552b7ab5",
+    "url": "/database-rick-n-morty/index.html"
+  },
+  {
+    "revision": "b299a367665f6231fb29",
+    "url": "/database-rick-n-morty/static/css/main.70544762.chunk.css"
+  },
+  {
+    "revision": "cf231bbd738d13119677",
+    "url": "/database-rick-n-morty/static/js/2.9a141841.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/database-rick-n-morty/static/js/2.9a141841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b299a367665f6231fb29",
+    "url": "/database-rick-n-morty/static/js/main.dad71a23.chunk.js"
+  },
+  {
+    "revision": "f7862e4ca9adfbfc58c4",
+    "url": "/database-rick-n-morty/static/js/runtime-main.e55fea5f.js"
+  },
+  {
+    "revision": "005e3536052db75217ed2f8550206c3e",
+    "url": "/database-rick-n-morty/static/media/logo-rick-n-morty.005e3536.png"
+  }
+]);
